f5613242a26631729e4af32d4ecc076ba8ec17ac
michifeli <mrojasr@usm.cl>
2025-07-31 06:27:43 -0400

    remove bug

--- .config/polybar/scripts/brightness.sh
+++ .config/polybar/scripts/brightness.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-# Encuentra el controlador de brillo. Puede variar (ej. intel_backlight, amdgpu_bl0)
-# Ajusta esto si 'acpi_video0' no funciona para ti.
 BRIGHTNESS_DEVICE=$(ls -d /sys/class/backlight/* | head -n 1 | xargs basename)
 
 case "$1" in
     up)
-        brightnessctl -d $BRIGHTNESS_DEVICE set +5% > /dev/null
+        brightnessctl -d "$BRIGHTNESS_DEVICE" set +5% > /dev/null
         ;;
     down)
-        brightnessctl -d $BRIGHTNESS_DEVICE set 5%- > /dev/null
+        brightnessctl -d "$BRIGHTNESS_DEVICE" set 5%- > /dev/null
         ;;
     *)
-        CURRENT_BRIGHTNESS=$(brightnessctl -d $BRIGHTNESS_DEVICE get)
-        MAX_BRIGHTNESS=$(brightnessctl -d $BRIGHTNESS_DEVICE max)
+        CURRENT_BRIGHTNESS=$(brightnessctl -d "$BRIGHTNESS_DEVICE" get)
+        MAX_BRIGHTNESS=$(brightnessctl -d "$BRIGHTNESS_DEVICE" max)
         PERCENTAGE=$((CURRENT_BRIGHTNESS * 100 / MAX_BRIGHTNESS))
-        echo "${PERCENTAGE}%%"
+
+        echo "Bri ${PERCENTAGE}%" # Solo texto y porcentaje
         ;;
 esac
